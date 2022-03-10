a8e188c5c82940199b5d7778bd8b173f72a28f25
Sven Gaubert <svengau@gmail.com>
2022-03-10 17:26:36 -0500

    add missing typescript types

---
+++ register.d.ts
@@ -0,0 +1,19 @@
+declare module "*.graphql" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.gql" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.gqls" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.graphqls" {
+  const value: string;
+  export = value;
+}
